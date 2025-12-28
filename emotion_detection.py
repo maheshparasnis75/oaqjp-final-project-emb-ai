c78697c8ac3e6e1da2463bd9523f357024bbad21
MAHESH PARASNIS <maheshparasnis75@gmail.com>
2025-12-27 20:06:55 -0500

    code changes for dominant emotion

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     # Define the URL for the emotion predict API
@@ -13,4 +14,20 @@ def emotion_detector(text_to_analyze):
     # Make a POST request to the API with the payload and headers
     response = requests.post(url, json=input_json, headers=header)
 
-    return response.text
+    formatted_response = json.loads(response.text)
+
+    emotionPredictions = formatted_response["emotionPredictions"][0]["emotion"]
+    
+    # Get the dominant emotion score
+    dominant_emotion_score = max(emotionPredictions.values())
+
+    # match the dominant emotion score with the corresponding emotion    
+    for emotion_key in emotionPredictions.keys():
+        if emotionPredictions[emotion_key] == dominant_emotion_score:
+            dominant_emotion = emotion_key
+            break
+
+    # add the dominant emotion in the response
+    emotionPredictions['dominant_emotion'] = dominant_emotion
+
+    return emotionPredictions
