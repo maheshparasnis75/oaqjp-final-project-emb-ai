86cc55ed5de90a336dd3eb74865d9d81948f3419
MAHESH PARASNIS <maheshparasnis75@gmail.com>
2025-12-27 23:23:28 -0500

    task8 code changes based on static code analysis

--- server.py
+++ server.py
@@ -1,3 +1,8 @@
+"""
+The server-side api functions, that
+- display user interface for input entry
+- initiate emotion detection analysis, upon user submit the form
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,6 +10,9 @@ app = Flask(__name__)
 
 @app.route("/emotionDetector")
 def emotion_detect():
+    """
+    The api function that initiates emotion detection analysis.
+    """
 
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
@@ -13,26 +21,31 @@ def emotion_detect():
     response = emotion_detector(text_to_analyze)
 
     out = "For the given statement, the system response is "
-    DOMINANT_EMOTION_KEY = "dominant_emotion"
+    dominant_emotion_key = "dominant_emotion"
 
     # error handling
-    if response[DOMINANT_EMOTION_KEY] is not None:
+    if response[dominant_emotion_key] is not None:
+        dominant_emotion_value = ""
         for emotion_key in response.keys():
-            if emotion_key == DOMINANT_EMOTION_KEY:
+            if emotion_key == dominant_emotion_key:
                 dominant_emotion_value = response[emotion_key]
             else:
                 out = out + "'" + emotion_key + "': " + str(response[emotion_key]) + ", "
-        
+
         #replace last comma with period
-        out = out[:out.rfind(",")] + "."        
+        out = out[:out.rfind(",")] + "."
         out = out + " The dominant emotion is " + "<b>" + dominant_emotion_value + "</b>" + "."
     else:
         out = "Invalid text! Please try again!"
-    
+
     return out
 
 @app.route("/")
 def render_index_page():
+    """
+    The api function that displays default landing page, that is user entry form.
+    """
+
     return render_template('index.html')
 
 if __name__ == "__main__":
