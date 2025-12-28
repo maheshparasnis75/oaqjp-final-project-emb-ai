7cbcb9ff0fce7daed64d5dd943c1399898f0bfb6
MAHESH PARASNIS <maheshparasnis75@gmail.com>
2025-12-27 21:40:06 -0500

    adding flask server class

---
+++ server.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask(__name__)
+
+@app.route("/emotionDetector")
+def emotion_detect():
+
+    # Retrieve the text to analyze from the request arguments
+    text_to_analyze = request.args.get('textToAnalyze')
+
+    # Pass the text to the emotion_detector function and store the response
+    response = emotion_detector(text_to_analyze)
+
+    out = "For the given statement, the system response is "
+    DOMINANT_EMOTION_KEY = "dominant_emotion"
+
+    for emotion_key in response.keys():
+        if emotion_key == DOMINANT_EMOTION_KEY:
+            dominant_emotion_value = response[emotion_key]
+        else:
+            out = out + "'" + emotion_key + "': " + str(response[emotion_key]) + ", "
+    
+    #replace last comma with period
+    out = out[:out.rfind(",")] + "."        
+    out = out + " The dominant emotion is " + "<b>" + dominant_emotion_value + "</b>" + "."
+    
+    return out
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
